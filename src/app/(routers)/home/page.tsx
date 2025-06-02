7b1d12ccccb5b2835d0a74cdd14853be444c9943
Deivid Gonçalves <deividcastro792@gmail.com>
2025-06-02 02:57:42 -0300

    pagina home

--- src/app/(routers)/home/page.tsx
+++ src/app/(routers)/home/page.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "@/styles/style-home.css";
 
+type MiniApp = {
+  id: string;
+  name: string;
+  description: string;
+  version: string;
+  entrypointUrl: string;
+  iconUrl: string;
+  repositoryUrl: string;
+  category: string;
+  permissions: string[];
+  creatorId: string;
+  createdAt: string;
+};
+
 export default function Home() {
+  const [miniApps, setMiniApps] = useState<MiniApp[]>([]);
+  const [showMiniApps, setShowMiniApps] = useState(false);
+  const [selectedMiniAppUrl, setSelectedMiniAppUrl] = useState<string | null>(null);
+  const closeIframe = () => setSelectedMiniAppUrl(null);
+
+  useEffect(() => {
+      localStorage.setItem("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZGI5MjQyNmUtYTk3OS00MTUzLTk0Y2YtOTBhYTFlNzQyZTVkIiwiaWF0IjoxNzQ3OTk1MDA1fQ.zsD6twtJQJFxNoC8q8ZQGlul8UTeB6e_CBY9zKUuYQE");
+    const token = localStorage.getItem("token");
+
+    fetch("http://localhost:3000/miniapps?page=1&limit=10", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data: MiniApp[]) => setMiniApps(data))
+      .catch((error) => console.error("Erro ao buscar miniApps:", error));
+  }, []);
   return (
-    <div className="iphone">
+     <div className="iphone">
       <div className="div">
         {/* Menu Superior */}
         <div className="overlap-home">
           <div className="status-bar"></div>
+
           {/* Nav Bar */}
           <div className="nav-bar-home">
             <div className="logo">
@@ -17,6 +50,7 @@ export default function Home() {
                 <div className="text-logo">SuperApp</div>
               </a>
             </div>
+
             {/* Valor */}
             <div className="home-valor">
               <div className="name-home">Olá, Fulano!</div>
@@ -25,64 +59,45 @@ export default function Home() {
               </a>
               <div className="group-2">
                 <div className="cifrão-home">R$</div>
-                <img
-                  className="ellipsis"
-                  src="/img/password-svgrepo-com.svg"
-                  alt="Password"
-                />
+                <img className="ellipsis" src="/img/password-svgrepo-com.svg" alt="Password" />
               </div>
             </div>
+
             {/* Pesquisa */}
             <div className="search-home">
               <div className="placeholder">
                 <div className="icon-search">
-                  <img
-                    className="img"
-                    src="/img/search-svgrepo-com.svg"
-                    alt="Search"
-                  />
+                  <img className="img" src="/img/search-svgrepo-com.svg" alt="Search" />
                 </div>
                 <input className="input" placeholder="Search" type="text" />
               </div>
             </div>
+
             {/* Opções do Navbar */}
             <div className="options-navbar">
-              {/* PIX */}
               <a href="/">
                 <div className="pix-button">
                   <div className="icone-pix">
-                    <img
-                      className="pix"
-                      src="/img/pix-svgrepo-com.svg"
-                      alt="PIX"
-                    />
+                    <img className="pix" src="/img/pix-svgrepo-com.svg" alt="PIX" />
                     <div className="text-wrapper-5">PIX</div>
                   </div>
                 </div>
               </a>
-              {/* Pagamentos */}
+
               <a href="/">
                 <div className="pag-button">
                   <div className="text-wrapper-6">Pagamentos</div>
-                  <img
-                    className="barcode-scanner"
-                    src="/img/bars-code-barcode-svgrepo-com.svg"
-                    alt="Pagamentos"
-                  />
+                  <img className="barcode-scanner" src="/img/bars-code-barcode-svgrepo-com.svg" alt="Pagamentos" />
                 </div>
               </a>
-              {/* Cartões */}
+
               <a href="/">
                 <div className="cards-button">
                   <div className="text-wrapper-7">Cartões</div>
-                  <img
-                    className="bank-cards"
-                    src="/img/cards-svgrepo-com.svg"
-                    alt="Cartões"
-                  />
+                  <img className="bank-cards" src="/img/cards-svgrepo-com.svg" alt="Cartões" />
                 </div>
               </a>
-              {/* Transporte */}
+
               <a href="/">
                 <div className="transporte-button">
                   <div className="text-wrapper-8">Transportes</div>
@@ -90,140 +105,207 @@ export default function Home() {
                 </div>
               </a>
             </div>
-            {/* Sair */}
+
             <a href="/">
               <div className="sair-button">Sair</div>
             </a>
           </div>
         </div>
+
         {/* Footer */}
         <div className="footer-home">
           <div className="overlap-2">
             <div className="bottom-nav">
               <a href="">
-                <img
-                  className="bottons-nav2"
-                  src="/img/home-4-svgrepo-com.svg"
-                  alt="Home"
-                />
+                <img className="bottons-nav2" src="/img/home-4-svgrepo-com.svg" alt="Home" />
               </a>
               <a href="/">
-                <img
-                  className="bottons-nav2"
-                  src="/img/notification-6-svgrepo-com.svg"
-                  alt="Notificações"
-                />
+                <img className="bottons-nav2" src="/img/notification-6-svgrepo-com.svg" alt="Notificações" />
               </a>
               <a href="/">
                 <div className="avatar">
-                  <img
-                    className="vector-2"
-                    src="/img/profile-circle-svgrepo-com.svg"
-                    alt="Perfil"
-                  />
+                  <img className="vector-2" src="/img/profile-circle-svgrepo-com.svg" alt="Perfil" />
                 </div>
               </a>
               <a href="">
                 <div className="vector-wrapper">
-                  <img
-                    className="vector-3"
-                    src="/img/message-square-dots-svgrepo-com.svg"
-                    alt="Mensagens"
-                  />
+                  <img className="vector-3" src="/img/message-square-dots-svgrepo-com.svg" alt="Mensagens" />
                 </div>
               </a>
               <a href="/">
-                <img
-                  className="bottons-nav2"
-                  src="/img/hamburger-menu-svgrepo-com.svg"
-                  alt="Menu"
-                />
+                <img className="bottons-nav2" src="/img/hamburger-menu-svgrepo-com.svg" alt="Menu" />
               </a>
             </div>
           </div>
         </div>
-        {/* Tabelas */}
-        <div className="tabs-home">
-          {/* obs: usar tab selected nas paginas atuais, unselected para as demais */}
-          <div className="tabs">
-            <a href="/">
-              <div className="tab-selected">
-                <div className="default">Banco</div>
-              </div>
-            </a>
-            <a href="/">
-              <div className="tab-unselected">
-                <div className="default">Social</div>
-              </div>
-            </a>
-            <a href="/">
-              <div className="tab-unselected">
-                <div className="default">MiniApps</div>
-              </div>
-            </a>
-            <a href="/">
-              <div className="tab-unselected">
-                <div className="default">Notícias</div>
-              </div>
-            </a>
-          </div>
+
+        {/* Tabelas e MiniApps */}
+       <div className="tabs-home">
+  <div className="tabs">
+    <div className="tabs-nav">
+      <a href="/">
+        <div className="tab-selected">
+          <div className="default">Banco</div>
         </div>
-        {/* Minha Conta */}
-        <a href="/">
-          <div className="account-button">
-            <div className="option">
-              <div className="pay-BTN">
-                <div className="title-options">MINHA CONTA</div>
-                <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
-              </div>
-            </div>
-          </div>
-        </a>
-        {/* Empréstimo */}
-        <a href="/">
-          <div className="emprestimo-button">
-            <div className="option">
-              <div className="pay-BTN">
-                <div className="title-options">EMPRÉSTIMO</div>
-                <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
-              </div>
-            </div>
-          </div>
-        </a>
-        {/* Investimento */}
-        <a href="/">
-          <div className="investimento-button">
-            <div className="option">
-              <div className="pay-BTN">
-                <div className="title-options">INVESTIMENTO</div>
-                <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
-              </div>
-            </div>
-          </div>
-        </a>
-        {/* Cartão de Crédito */}
-        <a href="/">
-          <div className="cc-button">
-            <div className="option">
-              <div className="pay-BTN">
-                <div className="title-options">CARTÃO DE CRÉDITO</div>
-                <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
-              </div>
-            </div>
-          </div>
-        </a>
-        {/* Cartão de Débito */}
-        <a href="/">
-          <div className="cd-button">
-            <div className="option">
-              <div className="pay-BTN">
-                <div className="title-options">CARTÃO DE DÉBITO</div>
-                <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
-              </div>
-            </div>
+      </a>
+
+      <a href="/">
+        <div className="tab-unselected">
+          <div className="default">Social</div>
+        </div>
+      </a>
+
+      <button
+        className={showMiniApps ? "tab-selected" : "tab-unselected"}
+        onClick={() => setShowMiniApps(true)}
+      >
+        <div className="default">MiniApps</div>
+      </button>
+
+      <a href="/">
+        <div className="tab-unselected">
+          <div className="default">Notícias</div>
+        </div>
+      </a>
+    </div>
+
+    {/* MiniApps visíveis quando ativados */}
+    {showMiniApps && (
+      <div className="gen-miniapp">
+        {miniApps.map((miniApp) => (
+  <button
+    key={miniApp.id}
+    onClick={() => setSelectedMiniAppUrl(miniApp.entrypointUrl)}
+    className="miniapps"
+    style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
+  >
+    <img
+      src={miniApp.iconUrl}
+      alt={`${miniApp.name} logo`}
+      className="miniapp-icon"
+    />
+  </button>
+))}
+
+
+        <a href="/register-MiniApp">
+          <div className="miniapps">
+            <img
+              src="./img/add-square-svgrepo-com.svg"
+              alt="Adicionar novo miniapp"
+              className="miniapp-icon"
+            />
           </div>
         </a>
       </div>
+    )}
+  </div>
+</div>
+
+
+        {/* Componentes visíveis quando showMiniApps for false */}
+        {!showMiniApps && (
+          <>
+            <a href="/">
+              <div className="account-button">
+                <div className="option">
+                  <div className="pay-BTN">
+                    <div className="title-options">MINHA CONTA</div>
+                    <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="/">
+              <div className="emprestimo-button">
+                <div className="option">
+                  <div className="pay-BTN">
+                    <div className="title-options">EMPRÉSTIMO</div>
+                    <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="/">
+              <div className="investimento-button">
+                <div className="option">
+                  <div className="pay-BTN">
+                    <div className="title-options">INVESTIMENTO</div>
+                    <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="/">
+              <div className="cc-button">
+                <div className="option">
+                  <div className="pay-BTN">
+                    <div className="title-options">CARTÃO DE CRÉDITO</div>
+                    <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="/">
+              <div className="cd-button">
+                <div className="option">
+                  <div className="pay-BTN">
+                    <div className="title-options">CARTÃO DE DÉBITO</div>
+                    <img className="icon-arrow" src="/img/arrow.svg" alt="Seta" />
+                  </div>
+                </div>
+              </div>
+            </a>
+          </>
+        )}
+      </div>
+      {selectedMiniAppUrl && (
+  <div
+    style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "rgba(0, 0, 0, 0.7)",
+      zIndex: 9999,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+    }}
+  >
+    <button
+      onClick={closeIframe}
+      style={{
+        marginBottom: "10px",
+        backgroundColor: "#fff",
+        border: "1px solid #ccc",
+        borderRadius: "4px",
+        padding: "8px 16px",
+        cursor: "pointer",
+      }}
+    >
+      Fechar MiniApp
+    </button>
+    <iframe
+      src={selectedMiniAppUrl}
+      style={{
+        width: "90vw",
+        height: "85vh",
+        border: "2px solid white",
+        borderRadius: "12px",
+        backgroundColor: "#fff",
+      }}
+      sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
+    ></iframe>
+  </div>
+)}
+
     </div>
   );
 }
